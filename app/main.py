6113b5d560dff862342a62c23f980d8fb8917a98
Danil <popovlfybk@yandex.ru>
2025-06-14 18:58:43 +0300

    Added deploy script

---
+++ app/main.py
@@ -0,0 +1,37 @@
+import api
+from settings import api_config, base_path
+import uvicorn
+import os
+
+def initialize_system() -> bool:
+    success = True
+    path = os.path.dirname(base_path)
+    temp_storage_path = os.path.join(path, os.path.join("app", "temp_storage"))
+    database_path = os.path.join(path, "database")
+
+    try:
+        os.makedirs(temp_storage_path, exist_ok=True)
+        os.makedirs(database_path, exist_ok=True)
+    except Exception:
+        success = False
+        print("Not all required directories were initialized")
+    
+    try:
+        # os.system(f"pip install -r {os.path.join(base_path, 'requirements.txt')}")
+        pass
+    except Exception:
+        success = False
+        print("Not all package were downloaded")
+
+    return success
+
+
+def main():
+    if not initialize_system():
+        return
+    
+    uvicorn.run(**api_config)
+
+
+if __name__ == '__main__':
+    main()
