e278e9d2dd300f2733ffe13cc502ffa3bc645666
Andrchest <andreipolevoi220@gmail.com>
2025-06-25 03:24:18 +0300

    initializer.py fix

--- app/initializer.py
+++ app/initializer.py
@@ -7,12 +7,14 @@ def initialize_system() -> bool:
     temp_storage_path = os.path.join(path, "app", "temp_storage")
     pdfs_path = os.path.join(path, "app", "temp_storage", "pdfs")
     database_path = os.path.join(path, "database")
+    chats_storage_path = os.path.join(path, "chats_storage")
 
     print(f"Base path: {base_path}")
     print(f"Parent path: {path}")
     print(f"Temp storage path: {temp_storage_path}")
     print(f"PDFs path: {pdfs_path}")
     print(f"Database path: {database_path}")
+    print(f"Database path: {chats_storage_path}")
 
     try:
         os.makedirs(temp_storage_path, exist_ok=True)
@@ -21,6 +23,8 @@ def initialize_system() -> bool:
         print("Created pdfs_path")
         os.makedirs(database_path, exist_ok=True)
         print("Created database_path")
+        os.makedirs(chats_storage_path, exist_ok=True)
+        print("Created chats_storage_path")
     except Exception as e:
         success = False
         print(f"Error creating directories: {str(e)}")
