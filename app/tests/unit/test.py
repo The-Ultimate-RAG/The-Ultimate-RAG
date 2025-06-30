398fd63c5e449ee6c1f99a1d261762b10c2ec158
Andrchest <andreipolevoi220@gmail.com>
2025-06-30 22:44:26 +0300

    coverage report + Codecov

--- app/tests/unit/test.py
+++ app/tests/unit/test.py
@@ -1,13 +1,16 @@
 import pytest
 from app.backend.controllers.schemas import SUser
 
+
 def test_password_no_digit():
     with pytest.raises(ValueError, match="Password must contain at least one number."):
         SUser(email="test@example.ru", password="NoDi@gitPassword!")
 
+
 def test_password_too_short():
     with pytest.raises(ValueError, match="String should have at least 8 characters"):
         SUser(email="test@example.ru", password="P1@Sal!")
+
 
 def test_password_too_long():
     with pytest.raises(ValueError, match="String should have at most 32 characters"):
@@ -16,9 +19,11 @@ def test_password_too_long():
             password="Strong.Password123!Strong.Password123!Strong.Password123!Strong.Password123!Strong.Passwor"
         )
 
+
 def test_email():
     with pytest.raises(ValueError, match="value is not a valid email address"):
         SUser(email="test.test", password="SoMeGrE@tPa22WoRd!")
+
 
 def test_valid_password():
     user = SUser(email="test@example.ru", password="Strong.Password123!")
